aeb25450b1bdcf127261880b8ac64388195d58ee
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2022-02-20 13:33:07 +0200

    Good bye SDKs

--- CloudHub.SDK/ClientInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CloudHub.SDK
-{
-    public struct ClientInfo
-    {
-        public string ClientKey { get; set; }
-        public string ClientSecret { get; set; }
-        public string ApiURL { get; set; }
-    }
-}
