52b72f9966373026cdc137b837bf74733419e0fb
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2022-02-20 23:53:46 +0200

    Much better app startup

--- CloudHub.API/Program.cs
+++ CloudHub.API/Program.cs
@@ -8,16 +8,15 @@ Configurations configurations = ConfigurationsLoader.Load("appsettings.json");
 builder.Services.AddSingleton<Configurations>(configurations);
 
 builder.RegisterDbContext(configurations);
+builder.ApplyDbMigrations(configurations);
 builder.RegisterFactories(configurations);
 builder.RegisterDomainServices(configurations);
 builder.Services.AddControllers(options => options.Filters.Add<ConsumerCredentialsFilter>());
 builder.ConfigureSwaggerServices(configurations);
-
 
 WebApplication app = builder.Build();
 app.ConfigureSwagger(configurations);
 app.UseMiddleware<ErrorMiddleware>();
 app.MapControllers();
 
-Migrations.ApplyPendingMigrations(configurations.MainConnectionString);
 app.Run();
---
+++ CloudHub.API/Startup/ServiceCollectionExtensions/Migrations.cs
@@ -0,0 +1,16 @@
+﻿using CloudHub.Infra.Data;
+
+namespace CloudHub.API.Startup
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static void ApplyDbMigrations(this WebApplicationBuilder builder, Configurations configurations)
+        {
+            IServiceProvider serviceProvider = builder.Services.BuildServiceProvider();
+            IServiceScopeFactory serviceScopeFactory = serviceProvider.GetService<IServiceScopeFactory>() ?? throw new Exception("Error while applying migrations, Failed to create Service Scope Factory");
+            IServiceScope serviceScope = serviceScopeFactory.CreateScope();
+            PostgreContext context = serviceScope.ServiceProvider.GetService<PostgreContext>() ?? throw new Exception("Error while applying migrations, Failed to get DbContext");
+            context.Database.EnsureCreated();
+        }
+    }
+}
